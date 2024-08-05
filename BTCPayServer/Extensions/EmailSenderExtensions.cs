d66e4194b1d16b1b5b315046724c9729a5028070
mr Smith <sima.kheirkhah@gmail.com>
2024-08-05 18:10:06 +0330

    confirmation email subject and body changed

--- BTCPayServer/Extensions/EmailSenderExtensions.cs
+++ BTCPayServer/Extensions/EmailSenderExtensions.cs
@@ -7,7 +7,7 @@ namespace BTCPayServer.Services
     public static class EmailSenderExtensions
     {
         private static string BODY_STYLE = "font-family: Open Sans, Helvetica Neue,Arial,sans-serif; font-color: #292929;";
-        private static string HEADER_HTML = "<h1 style='font-size:1.2rem'>BTCPay Server</h1><br/>";
+        private static string HEADER_HTML = "<h1 style='font-size:1.2rem'>DCO Gateway</h1><br/>";
         private static string BUTTON_HTML = "<a href='{button_link}' type='submit' style='min-width: 2em;min-height: 20px;text-decoration-line: none;cursor: pointer;display: inline-block;font-weight: 400;color: #fff;text-align: center;vertical-align: middle;user-select: none;background-color: #51b13e;border-color: #51b13e;border: 1px solid transparent;padding: 0.375rem 0.75rem;font-size: 1rem;line-height: 1.5;border-radius: 0.25rem;transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;'>{button_description}</a>";
 
         private static string CallToAction(string actionName, string actionLink)
@@ -36,7 +36,7 @@ namespace BTCPayServer.Services
         public static void SendResetPassword(this IEmailSender emailSender, MailboxAddress address, string link)
         {
             emailSender.SendEmail(address, "Update Password", CreateEmailBody(
-                $"A request has been made to reset your BTCPay Server password. Please set your password by clicking below.<br/><br/>{CallToAction("Update Password", link)}"));
+                $"A request has been made to reset your DCO Gateway password. Please set your password by clicking below.<br/><br/>{CallToAction("Update Password", link)}"));
         }
 
         public static void SendInvitation(this IEmailSender emailSender, MailboxAddress address, string link)
